f7b9c6d5711901d230936819257ac180fff04294
Ruslan Zotsenko <justmymail989@gmail.com>
2024-05-17 21:31:11 +0200

    My calculate

---
+++ js/task-1.js
@@ -0,0 +1,35 @@
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const lastChar = display.value.slice(-1);
+    if (['+', '-', '*', '/'].includes(lastChar)) {
+        deleteLast();
+    }
+    display.value += operator;
+}
+
+function appendDot() {
+    if (!display.value.includes('.')) {
+        display.value += '.';
+    }
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'Error';
+    }
+}
